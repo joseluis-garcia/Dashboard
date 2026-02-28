d15c17c95c4821d47393e685770eb34d84125307
joseluis-garcia <71826340+joseluis-garcia@users.noreply.github.com>
2026-02-28 13:37:57 +0100

    Update app_Tomorrow.py

--- apps/tomorrow/app_Tomorrow.py
+++ apps/tomorrow/app_Tomorrow.py
@@ -3,7 +3,7 @@ import streamlit as st
 import sys
 from pathlib import Path
 
-repo_root = Path(__file__).resolve().parents[1]
+repo_root = Path(__file__).resolve().parents[2]
 st.write("Current working directory:", os.getcwd())
 st.write("Files in root:", os.listdir("/mount/src"))
 st.write("Repo root:", repo_root)
